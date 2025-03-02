f28169c5174d5f74ffefc1918a7f8a7844e9f1e5
andrulonis <48800417+andrulonis@users.noreply.github.com>
2025-03-02 17:46:40 +0100

    clean up strategy and naming

--- aal/aal/adaptation_strategies/prism_adaptation_strategy.py
+++ aal/aal/adaptation_strategies/prism_adaptation_strategy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 import subprocess
+import os
 
 from aal.adaptation_strategies.adaptation_strategy import AdaptationStrategy
 
@@ -15,43 +16,45 @@ class PrismStrategy(AdaptationStrategy):
         print(f"safety: {adaptation_state.safeties}")
         print(f"power: {adaptation_state.powers}")
         print(f"move: {adaptation_state.moves}")
-        possible_configs = adaptation_state.possible_configurations
-        prism_dir = "~/rebet_ws/prism-4.8.1-linux64-x86/bin/prism"
-        model = '~/rebet_ws/src/aal/aal/aal/adaptation_strategies/models/model.pm' 
-        props = '~/rebet_ws/src/aal/aal/aal/adaptation_strategies/models/base_mission.pctl' 
+        
+        prism_bin = "~/rebet_ws/prism-4.8.1-linux64-x86/bin/prism"
+        models_path = '~/rebet_ws/src/aal/aal/aal/adaptation_strategies/models'
+        full_models_path = os.path.expanduser(models_path)
 
+        possible_configs = adaptation_state.possible_configurations
         best_config = possible_configs[0]
         best_util = float('-inf')
 
-        abs_path = '/home/andrulonis/rebet_ws/src/aal/aal/aal/adaptation_strategies/models' #TODO: fix the pathing
-
-        with open(f'{abs_path}/base_mission.pm','r') as base_model_file:
+        with open(f'{full_models_path}/base_model.pm','r') as base_model_file:
             base_model = base_model_file.read()
 
         for config in possible_configs:
             speed = config.configuration_parameters[0].value.double_array_value[0]
 
-            with open(f'{abs_path}/model.pm','w') as model_file:
+            # TODO: consider what to do at the beginning/when the values from frog_adapt_nodes (utility_of_adaptation()) come as 0.0 or negative
+            # Fill in monitored/config-speicifc data into model
+            with open(f'{full_models_path}/final_model.pm','w') as model_file:
                 model_file.write(base_model)
-                model_file.write(f'const double safety = {adaptation_state.safeties[0]};')
-                model_file.write(f'const double power = {adaptation_state.powers[0]};')
-                model_file.write(f'const double move = {adaptation_state.moves[0]};')
-                model_file.write(f'const double speed = {speed};')
+                model_file.write(f'const double safety = {adaptation_state.safeties[0]};\n')
+                model_file.write(f'const double power = {adaptation_state.powers[0]};\n')
+                model_file.write(f'const double move = {adaptation_state.moves[0]};\n')
+                model_file.write(f'const double speed = {speed};\n')
                 
-            # Run PRSIM in subprocess
+            # Run PRISM for config
             completed_process = subprocess.run(
-                [f'~/rebet_ws/prism-4.8.1-linux64-x86/bin/prism {model} {props}'],
+                [f'{prism_bin} {models_path}/final_model.pm {models_path}/properties.pctl'],
                 shell=True, capture_output=True, text=True)
 
             # Parse output
             prop_results = []
             output = completed_process.stdout
 
+            # Put results for each property in an array
             for result_string in output.split("Result: ")[1:]:
                 prop_results.append(float(result_string.split()[0]))
 
             print(prop_results)
-            util = speed # TODO: rethink
+            util = speed # TODO: rethink, maybe the utility can be calculated in the model as a property rather than here
             if util > best_util:
                 best_config = config
                 best_util = util
