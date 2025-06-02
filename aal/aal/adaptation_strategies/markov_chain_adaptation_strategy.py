d65334502062b7023698a057786b1b722206f0bb
andrulonis <48800417+andrulonis@users.noreply.github.com>
2025-06-02 16:45:41 +0200

    Abstract and cleanup strategies
    Co-authored-by: Daanvduin <Daanvduin@users.noreply.github.com>

--- aal/aal/adaptation_strategies/markov_chain_adaptation_strategy.py
+++ aal/aal/adaptation_strategies/markov_chain_adaptation_strategy.py
@@ -5,109 +5,136 @@ import sys
 
 from aal.adaptation_strategies.adaptation_strategy import AdaptationStrategy
 
+# Get path to PRISM binary
+prism_bin = "~/rebet_ws/src/aal/prism-4.8.1-linux64-x86/bin/prism"
+
 class PrismMarkovChainStrategy(AdaptationStrategy):
 
     def __init__(self):
         super().__init__('markov_chain')
- 
+
+    # Store possible values of string parameters in a dictionary
+    def handle_string_config_params(self):
+        self.str_vars = {}
+        if os.path.isfile(f"{self.model_dir}/string_config_params.txt"):
+            with open(f'{self.model_dir}/string_config_params.txt', 'r') as string_config_params_file:
+                for var in string_config_params_file:
+                    split_line = var.split()
+                    if len(split_line[1:]) <= 1:
+                        print(f"Too few possible values for string variable: {split_line[0]}")
+                        return False
+                    self.str_vars[split_line[0]] = split_line[1:]
+        return True
+
+    # Check if all necessary files are provided
+    def check_files_present(self):
+        if not os.path.exists(self.model_dir):
+            print(f"Path {self.model_dir} does not exist, please provide a correct path.")
+            return False
+        
+        if not os.path.isfile(f"{self.model_dir}/base_model.pm"):
+            print(f"File base_model.pm is missing from the given directory.")
+            return False
+            
+        if not os.path.isfile(f"{self.model_dir}/properties.pctl"):
+            print(f"File properties.pctl is missing from the given directory.")
+            return False
+            
+        if not os.path.isfile(f"{self.model_dir}/utility_function.py"):
+            print(f"File utility_function.py is missing from the given directory.")
+            return False
+        return True
+    
+    # Write qr-metrics and context values to the model (all assumed to be given as doubles)
+    def write_qr_and_context_to_model(self, qrs, context):
+        with open(f'{self.model_dir}/base_model.pm','r') as base_model_file:
+            self.final_model = base_model_file.read()
+        for qr in qrs:
+            self.final_model += f'\nconst double {qr.qr_name.lower()} = {qr.metric};'
+        for kv in context:
+            self.final_model += f'\nconst double {kv.key.lower()} = {kv.value};'
+
+    # Fill in configuration parameters in model
+    def write_config_to_model(self, config_params):
+        with open(f'{self.model_dir}/final_model.pm','w') as model_file:
+            model_file.write(self.final_model)
+            for param in config_params:
+                if param.value.type == 1:   # Case of bool
+                    model_file.write(f'\nconst bool {param.name} = {str(param.value.bool_value).lower()};')
+                elif param.value.type == 2: # Case of integer
+                    model_file.write(f'\nconst int {param.name} = {param.value.integer_value};')
+                elif param.value.type == 3: # Case of double
+                    model_file.write(f'\nconst double {param.name} = {param.value.double_value};')
+                elif param.value.type == 4: # Case of string
+                    # PRISM does not support strings, so instead write the index of the value
+                    if param.name not in self.str_vars or param.value.string_value not in self.str_vars[param.name]:
+                        print(f"Value given to string {param.name} not listed as possible value")
+                        return False
+                    model_file.write(f'\nconst int {param.name} = {self.str_vars[param.name].index(param.value.string_value)};')
+                elif param.value.type == 5: # Case of byte array
+                    for i, val in enumerate(param.value.byte_array_value):
+                        model_file.write(f'\nconst int {param.name}_{i} = {val};')
+                elif param.value.type == 6: # Case of bool array
+                    for i, val in enumerate(param.value.bool_array_value):
+                        model_file.write(f'\nconst bool {param.name}_{i} = {str(val).lower()};')
+                elif param.value.type == 7: # Case of integer array
+                    for i, val in enumerate(param.value.integer_array_value):
+                        model_file.write(f'\nconst int {param.name}_{i} = {val};')
+                elif param.value.type == 8: # Case of double array
+                    for i, val in enumerate(param.value.double_array_value):
+                        model_file.write(f'\nconst double {param.name}_{i} = {val};')
+                elif param.value.type == 9: # Case of string array
+                    for i, val in enumerate(param.value.string_array_value):
+                        # PRISM does not support strings, so instead write the index of the value, with added suffix of "_i", where i is the index within the array received
+                        if param.name not in self.str_vars or val not in self.str_vars[param.name]:
+                            print(f"Value given to string {param.name}_{i} not listed as possible value")
+                            return False
+                        model_file.write(f'\nconst int {param.name}_{i} = {self.str_vars[param.name].index(val)};')
+        return True
+
     def suggest_adaptation(self, adaptation_state):
         # Get path to the model, property from the user (from the behaviour tree)
-        model_dir = adaptation_state.model_dir
+        self.model_dir = adaptation_state.model_dir
 
         # Initially set chosen configuration to the first one provided
         possible_configs = adaptation_state.possible_configurations
         chosen_config = possible_configs[0]
         best_util = float('-inf')
 
-        if not os.path.exists(model_dir):
-            print(f"Path {model_dir} does not exist, please provide a correct path.")
-            return chosen_config
-        
-        if not os.path.isfile(f"{model_dir}/base_model.pm"):
-            print(f"File base_model.pm is missing from the given directory.")
+        if not self.check_files_present():
             return chosen_config
             
-        if not os.path.isfile(f"{model_dir}/properties.pctl"):
-            print(f"File properties.pctl is missing from the given directory.")
-            return chosen_config
-            
-        if not os.path.isfile(f"{model_dir}/required_vars.txt"):
-            print(f"File required_vars.txt is missing from the given directory.")
-            return chosen_config
-        
-        if not os.path.isfile(f"{model_dir}/utility_function.py"):
-            print(f"File utility_function.py is missing from the given directory.")
+        if not self.handle_string_config_params():
             return chosen_config
 
         # Import the utility function from the model directory
-        sys.path.append(model_dir)
+        sys.path.append(self.model_dir)
         from utility_function import calculate_utility
-        
-        # Get path to PRISM program
-        prism_bin = "~/rebet_ws/src/aal/prism-4.8.1-linux64-x86/bin/prism"
 
-        # Check if all required parametrised PRISM variables are provided (sometimes not the case in first few iterations),
-        # and in case of strings, store possible values of the string in a dictionairy
-        str_vars = {}
-        keys = [kv.key.lower() for kv in adaptation_state.context] + \
-               [qr.qr_name.lower() for qr in adaptation_state.qrs] + \
-               [param.name for param in possible_configs[0].configuration_parameters]
-        
-        with open(f'{model_dir}/required_vars.txt', 'r') as required_vars_file:
-            for var in required_vars_file:
-                split_line = var.split()
-                # Regular case of variable written straight to the model
-                if len(split_line) == 1:
-                    if var.strip() not in keys:
-                        print(f"Important context is missing: {var}")
-                        return chosen_config
-                # String case with possible values
-                else:
-                    if split_line[0] not in keys:
-                        print(f"Important context is missing: {split_line[0]}")
-                        return chosen_config
-                    else:
-                        if len(split_line[1:]) <= 1:
-                            print(f"Too few possible values for string variable: {split_line[0]}")
-                            return chosen_config
-                        str_vars[split_line[0]] = split_line[1:]
+        qrs = adaptation_state.qrs
+        context = adaptation_state.context
 
-        # Write qr-metrics and context values to the model (all assumed to be given as doubles)
-        with open(f'{model_dir}/base_model.pm','r') as base_model_file:
-            base_model = base_model_file.read()
-        for qr in adaptation_state.qrs:
-            base_model += f'\nconst double {qr.qr_name.lower()} = {qr.metric};'
-        for kv in adaptation_state.context:
-            base_model += f'\nconst double {kv.key.lower()} = {kv.value};'
+        self.write_qr_and_context_to_model(qrs, context)
         
         for config in possible_configs:
-            # Fill in configuration parameters in model
-            with open(f'{model_dir}/final_model.pm','w') as model_file:
-                model_file.write(base_model)
-                for param in config.configuration_parameters:
-                    if param.value.type == 1:   # Case of bool
-                        model_file.write(f'\nconst bool {param.name} = {str(param.value.bool_value).lower()};')
-                    elif param.value.type == 2: # Case of integer
-                        model_file.write(f'\nconst int {param.name} = {param.value.integer_value};')
-                    elif param.value.type == 3: # Case of double
-                        model_file.write(f'\nconst double {param.name} = {param.value.double_value};')
-                    elif param.value.type == 4: # Case of string
-                        # PRISM does not support strings, so instead write the index of the value
-                        if param.value.string_value not in str_vars[param.name]:
-                            print(f"Value given to string {param.name} not listed as possible value")
-                        model_file.write(f'\nconst int {param.name} = {str_vars[param.name].index(param.value.string_value)};')
-                    # TODO: handle the array types as well
+            if not self.write_config_to_model(config.configuration_parameters):
+                return chosen_config
                 
             # Run PRISM for config
             completed_process = subprocess.run(
-                [f'{prism_bin} {model_dir}/final_model.pm {model_dir}/properties.pctl'],
+                [f'{prism_bin} {self.model_dir}/final_model.pm {self.model_dir}/properties.pctl'],
                 shell=True, capture_output=True, text=True)
 
             # Parse output
             prop_results = []
             output = completed_process.stdout
-            print(output)
-            for result_string in output.split("Result: ")[1:]:
+            results = output.split("Result: ")[1:]
+            
+            if results == [output]:
+                print("PRISM did not give expected output. There may be a syntax error in the model, or some parameters did not get written properly.")
+                return chosen_config
+            
+            for result_string in results:
                 value_string = result_string.split()[0]
                 # Depending on the property, result may be a bool, int or float
                 if value_string == "true":
@@ -115,9 +142,9 @@ class PrismMarkovChainStrategy(AdaptationStrategy):
                 elif value_string == "false":
                     prop_results.append(False)
                 elif value_string.isdigit():
-                     prop_results.append(int(value_string))
+                    prop_results.append(int(value_string))
                 else:
-                     prop_results.append(float(value_string))
+                    prop_results.append(float(value_string))
 
             # Use provided function to get utility of the adaptation, update chosen config if this one is better
             util = calculate_utility(prop_results)
--- aal/aal/adaptation_strategies/mdp_adaptation_strategy.py
+++ aal/aal/adaptation_strategies/mdp_adaptation_strategy.py
@@ -5,84 +5,66 @@ import sys
 
 from aal.adaptation_strategies.adaptation_strategy import AdaptationStrategy
 
+# Get path to PRISM binary
+prism_bin = "~/rebet_ws/src/aal/prism-4.8.1-linux64-x86/bin/prism"
+
 class PrismMDPStrategy(AdaptationStrategy):
 
     def __init__(self):
         super().__init__('mdp')
+
+    # Check if all necessary files are provided
+    def check_files_present(self):
+        if not os.path.exists(self.model_dir):
+            print(f"Path {self.model_dir} does not exist, please provide a correct path.")
+            return False
+        
+        if not os.path.isfile(f"{self.model_dir}/base_model.pm"):
+            print(f"File base_model.pm is missing from the given directory.")
+            return False
+            
+        if not os.path.isfile(f"{self.model_dir}/property.pctl"):
+            print(f"File property.pctl is missing from the given directory.")
+            return False
+        return True
+    
+    # Write qr-metrics and context values to the model (all assumed to be given as doubles) and state variables (config) as initial values (assumed to be given as ints)
+    def write_qr_and_context_and_config_to_model(self, qrs, context, config):
+        with open(f'{self.model_dir}/base_model.pm','r') as base_model_file:
+            self.final_model = base_model_file.read()
+        for qr in qrs:
+            self.final_model += f'\nconst double {qr.qr_name.lower()} = {qr.metric};'
+        for kv in context:
+            self.final_model += f'\nconst double {kv.key.lower()} = {kv.value};'
+        for kv in config:
+            self.final_model += f'\nconst {kv.key.lower()}_init = {kv.value};'
  
     def suggest_adaptation(self, adaptation_state):
         # Get path to the model, property from the user (from the behaviour tree)
-        model_dir = adaptation_state.model_dir
+        self.model_dir = adaptation_state.model_dir
 
         # Initially set chosen configuration to the first one provided
         possible_configs = adaptation_state.possible_configurations
         chosen_config = possible_configs[0]
 
-        if not os.path.exists(model_dir):
-            print(f"Path {model_dir} does not exist, please provide a correct path.")
+        if not self.check_files_present():
             return chosen_config
-        
-        if not os.path.isfile(f"{model_dir}/base_model.pm"):
-            print(f"File base_model.pm is missing from the given directory.")
-            return chosen_config
-            
-        if not os.path.isfile(f"{model_dir}/property.pctl"):
-            print(f"File property.pctl is missing from the given directory.")
-            return chosen_config
-            
-        is_req_vars_present = os.path.isfile(f"{model_dir}/required_vars.txt")
-        
-        # Get path to PRISM program
-        prism_bin = "~/rebet_ws/src/aal/prism-4.8.1-linux64-x86/bin/prism"
 
-        with open(f'{model_dir}/property.pctl') as property_file:
+        with open(f'{self.model_dir}/property.pctl') as property_file:
             prop = property_file.readline()
 
-        # Check if all required parametrised PRISM variables are provided (sometimes not the case in first few iterations),
-        # and in case of strings, store possible values of the string in a dictionairy
-        str_vars = {}
-        keys = [kv.key.lower() for kv in adaptation_state.context] + \
-               [qr.qr_name.lower() for qr in adaptation_state.qrs] + \
-               [param.name for param in possible_configs[0].configuration_parameters]
-        
-        if is_req_vars_present:
-            with open(f'{model_dir}/required_vars.txt', 'r') as required_vars_file:
-                for var in required_vars_file:
-                    split_line = var.split()
-                    # Regular case of variable written straight to the model
-                    if len(split_line) == 1:
-                        if var.strip() not in keys:
-                            print(f"Important context is missing: {var}")
-                            return chosen_config
-                    # String case with possible values
-                    else:
-                        if split_line[0] not in keys:
-                            print(f"Important context is missing: {split_line[0]}")
-                            return chosen_config
-                        else:
-                            if len(split_line[1:]) <= 1:
-                                print(f"Too few possible values for string variable: {split_line[0]}")
-                                return chosen_config
-                            str_vars[split_line[0]] = split_line[1:]
+        qrs = adaptation_state.qrs
+        context = adaptation_state.context
+        config = adaptation_state.config
 
-        # Write qr-metrics and context values to the model (all assumed to be given as doubles)
-        with open(f'{model_dir}/base_model.pm','r') as base_model_file:
-            base_model = base_model_file.read()
-        for qr in adaptation_state.qrs:
-            base_model += f'\nconst double {qr.qr_name.lower()} = {qr.metric};'
-        for kv in adaptation_state.context:
-            base_model += f'\nconst double {kv.key.lower()} = {kv.value};'
-            
-        # Write state variables (config) to model as initial values (assumed to be given as ints)
-        for kv in adaptation_state.config:
-            base_model += f'\nconst {kv.key.lower()}_init = {kv.value};'
+        self.write_qr_and_context_and_config_to_model(qrs, context, config)
 
-        with open(f'{model_dir}/final_model.pm','w') as model_file:
-            model_file.write(base_model)
+        with open(f'{self.model_dir}/final_model.pm','w') as model_file:
+            model_file.write(self.final_model)
         
         # Generate optimal strategy optimising over specified property
         completed_process = subprocess.run(
-            [f'{prism_bin} {model_dir}/final_model.pm -pf \'{prop}\' -exportstrat stdout:reach=false'],
+            [f'{prism_bin} {self.model_dir}/final_model.pm -pf \'{prop}\' -exportstrat stdout:reach=false'],
             shell=True, capture_output=True, text=True)
         
         # Parse output to obtain strategy
@@ -91,6 +73,7 @@ class PrismMDPStrategy(AdaptationStrategy):
         strategy_string = output.split("Exporting strategy as actions below:\n")[1].split("\n---")[0]
 
         if strategy_string == [output]:
+            print("PRISM did not give expected output. There may be a syntax error in the model, or some parameters did not get written properly.")
             return chosen_config
 
         for line in strategy_string.splitlines():
